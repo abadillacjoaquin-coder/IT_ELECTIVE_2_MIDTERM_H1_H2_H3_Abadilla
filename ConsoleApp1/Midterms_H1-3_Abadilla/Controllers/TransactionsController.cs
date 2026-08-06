a2fe4d4caf7d3a0407cd43f8ad70c3f23e6f80f9
CHRISTOPHER JOAQUIN ABADILLA <C1891-24@itmlyceumalabang.onmicrosoft.com>
2026-08-07 03:20:38 +0800

    feat: Add checkout processing and transaction history (US-05 and US-06)

---
+++ ConsoleApp1/Midterms_H1-3_Abadilla/Controllers/TransactionsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Midterms_H1_3_Abadilla.Controllers
+{
+    public class TransactionsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ConsoleApp1/Midterms_H1-3_Abadilla/Models/Domain/Transaction.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Midterms_H1_3_Abadilla.Models.Domain
+{
+    public class Transaction : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
